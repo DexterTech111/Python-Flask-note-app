e776cccb62389b879a20bffc0b2b357eb36a2ba5
DexterTech <62775645+DexterTech111@users.noreply.github.com>
2024-04-09 08:15:53 -0700

    included alchemy and db modules

--- website/__init__.py
+++ website/__init__.py
@@ -2,7 +2,8 @@ from flask import Flask
 
 def create_app():
     app = Flask(__name__)
-    app.config['SERVER_KEY'] = 'hgjhghjghjxfsdfsdfsd' #helps secure cookies and session data 
+    app.config['SERVER_KEY'] = '' #helps secure cookies and session data 
+    app.secret_key = b'_5#y2L"F4Q8z\n\xec]/' 
     
     from .views import views
     from .auth import auth
--- website/auth.py
+++ website/auth.py
@@ -1,9 +1,11 @@
-from flask import Blueprint, render_template #help us organize our files
+from flask import Blueprint, render_template, request, flash  #help us organize our files
 
 auth = Blueprint('auth', __name__)
 
-@auth.route('/login')
+@auth.route('/login', methods=['GET', 'POST'])
 def login():
+    data = request.form
+    print(data)
     return render_template("login.html")
     
 @auth.route('/logout')
@@ -11,6 +13,24 @@ def logout():
     return '<p>logout</p>'
     
     
-@auth.route('/sign-up')
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def signup():
+    if request.method == 'POST':
+        name = request.form.get('name')
+        email = request.form.get('email')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+        
+        if len(email) < 4:
+            flash('Email is too short.', category='error')
+        elif len(name) < 2:
+            flash('Name is too short', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 4:
+            flash('The length of your password should be above 4 characters', category='error')
+        else:
+            #add to database
+            flash('all is good', category='success')
+   
     return render_template("signup.html")
