dbd5a097a56dc9198a8fa83a094d0029ef1df20c
DexterTech <62775645+DexterTech111@users.noreply.github.com>
2024-04-16 03:49:42 -0700

    updated main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from website import create_app
 
+
 app = create_app()
 
 if __name__ == '__main__': #this below code should execute only if we run this file as the main script and not when we import the this file another file
